34049c7b9a7ee1f3485b32cfd92eb0c02eac4b3d
EvgeneOskin <eoskin@crystalnix.com>
2016-10-27 18:51:31 +0600

    Add serving static.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 ./manage.py migrate --noinput
 ./manage.py collectstatic --noinput
-uwsgi --http :80 --chdir "$PWD" --wsgi-file animals/wsgi.py --check-static "$PWD/static"
+uwsgi --http :80 --chdir "$PWD" --wsgi-file animals/wsgi.py --static-map '/static'="$PWD/static/"
